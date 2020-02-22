e9d4728303a237e9630d5a902c048636d9d68ad8
ilushishe <ilushishe@gmail.com>
2020-02-22 14:13:37 -0500

    pytest.ini added

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def pytest_addoption(parser):
     parser.addoption("--language", action="store", default="en", help="Choose language")
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
     options = Options()
--- test_product_page.py
+++ test_product_page.py
@@ -27,6 +27,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_presented_succeed_message()
 
+    @pytest.mark.need_review
     @pytest.mark.parametrize('link',
                              ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -40,7 +41,7 @@ class TestUserAddToBasketFromProductPage():
                                   marks=pytest.mark.xfail(reason="minor bug")),
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-    def test_user_can_add_product_to_basket(slef, browser, link):
+    def test_user_can_add_product_to_basket(self, browser, link):
         link = link
         page = ProductPage(browser, link)
         page.open()
@@ -58,6 +59,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_presented_succeed_message()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link',
                          ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                           "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -109,6 +111,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -116,6 +119,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
